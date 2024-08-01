912a843bea37e469c7cb1719d42a78f46f8e4744
Shankar Damodharan <shankardamodharan1998@gmail.com>
2024-08-01 22:26:33 +0530

    Authentication and Authorization

--- employee_register/admin.py
+++ employee_register/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
-from employee_register.model import Employee, Position
-# Register your models here.
+from employee_register.model import Employee, Position, Product
 
 admin.site.register(Employee)
 admin.site.register(Position)
 
+class ProductMixIn:
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
+    def has_view_permission(self, request, obj=None):
+        return True
+
+@admin.register(Product)
+class ProductAdmin(ProductMixIn, admin.ModelAdmin):
+    list_display = ("name",)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+
+        if not is_superuser:
+            form.base_fields['name'].disabled = True
+        
+        return form
